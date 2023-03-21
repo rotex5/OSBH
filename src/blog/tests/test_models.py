e5c3de81f3c4e7ab7b5d56b5f608bb43908def2a
Kaditcuy <philipukchimaobi@gmail.com>
2023-03-21 05:51:01 +0000

    Added unittest to test the blog models

--- src/blog/tests/test_models.py
+++ src/blog/tests/test_models.py
@@ -1,31 +1,89 @@
 from django.test import TestCase
+from django.urls import reverse
 from django.contrib.auth.models import User
-from blog.models import BlogAuthor, Blog, BlogComment, BlogView, BlogLike
-import sys
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-class BlogAuthorModelTest(TestCase):
-
-    sys.stdout.write('Test')
+from .models import Blog, BlogAuthor, BlogComment, BlogView, BlogLike
 
 
-    @classmethod
-    def setupTestData(cls):
-        test_user1 = User.objects.create_user(username='testuser1', password='12345')
-        test_user1.save()
-        #BlogAuthor.objects.create(user=test_user1, bio='Funfact: Im the second blog author')
+class BlogModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='password')
+        self.author = BlogAuthor.objects.create(user=self.user, bio='test bio')
+        self.blog = Blog.objects.create(title='test title', content='test content', author=self.author)
 
-    @classmethod
-    @receiver(post_save, sender = User)
-    def user_is_created(sender, instance, created, **kwargs):
-        """ When any user instance created, FileUploader object
-        instance is created and automatically linked by User """
-        if created:
-            BlogAuthor.objects.create(user=instance)
-        else:
-            instance.blogauthor.save()
+    def test_blog_str(self):
+        self.assertEqual(str(self.blog), 'test title')
 
-    def test_get_absolute_url(self):
-        author=BlogAuthor.objects.get(id=3)
-        self.assertEquals(author.get_absolute_url(), '/blog/author/3')
+    def test_blog_absolute_url(self):
+        url = reverse('blog-detail', args=[str(self.blog.id)])
+        self.assertEqual(self.blog.get_absolute_url(), url)
+
+    def test_blog_like_url(self):
+        url = reverse('blog-like', args=[str(self.blog.id)])
+        self.assertEqual(self.blog.get_like_url(), url)
+
+    def test_blog_comment_count(self):
+        BlogComment.objects.create(blog=self.blog, content='test comment', commenter=self.user)
+        self.assertEqual(self.blog.get_comment_count, 1)
+
+    def test_blog_view_count(self):
+        BlogView.objects.create(blog=self.blog, user=self.user)
+        self.assertEqual(self.blog.get_view_count, 1)
+
+    def test_blog_like_count(self):
+        BlogLike.objects.create(post=self.blog, user=self.user)
+        self.assertEqual(self.blog.get_like_count, 1)
+
+
+class BlogAuthorModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='password')
+        self.author = BlogAuthor.objects.create(user=self.user, bio='test bio')
+
+    def test_author_str(self):
+        self.assertEqual(str(self.author), 'testuser')
+
+    def test_author_absolute_url(self):
+        url = reverse('author-detail', args=[str(self.author.id)])
+        self.assertEqual(self.author.get_absolute_url(), url)
+
+
+class BlogCommentModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='password')
+        self.author = BlogAuthor.objects.create(user=self.user, bio='test bio')
+        self.blog = Blog.objects.create(title='test title', content='test content', author=self.author)
+        self.comment = BlogComment.objects.create(blog=self.blog, content='test comment', commenter=self.user)
+
+    def test_comment_str(self):
+        self.assertEqual(str(self.comment), 'test comment')
+
+    def test_comment_ordering(self):
+        comment2 = BlogComment.objects.create(blog=self.blog, content='test comment 2', commenter=self.user)
+        self.assertLess(self.comment.timestamp, comment2.timestamp)
+
+
+class BlogViewModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='password')
+        self.author = BlogAuthor.objects.create(user=self.user, bio='test bio')
+        self.blog = Blog.objects.create(title='test title', content='test content', author=self.author)
+
+    def test_view_str(self):
+        view = BlogView.objects.create(blog=self.blog, user=self.user)
+        self.assertEqual(str(view), 'testuser')
+
+
+class BlogLikeModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='password')
+        self.author = BlogAuthor.objects.create(user=self.user, bio='test bio')
+        self.blog = Blog.objects.create(title='test title', content='test content', author=self.author)
+
+    def test_like_str(self):
+        like = BlogLike.objects.create(post=self.blog, user=self.user)
+        self.assertEqual(str(like), 'testuser')
+
+
+class SignalTestCase(TestCase):
+    def test_blogauthor_created
+
